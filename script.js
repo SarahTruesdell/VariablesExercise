933da588f0a7f9fef3cd65487d7566f87758c162
“SarahTruesdell” <s.truesdell87@outlook.com>
2022-03-11 11:08:27 -0500

    VariablesExercise

---
+++ script.js
@@ -0,0 +1,27 @@
+//const
+
+let num = 20;
+num = 15;
+num = num + 7;
+num --;
+num = "This is not a number"
+
+let blank;
+console.log(blank)
+blank = null;
+
+// Bonus
+//1, 2
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// or 
+// Const [quarter, dime, nickle, penny] = [0.25, 0.10, 0.05, 0.01]
+
+console.log(quarter, dime, nickel, penny)
+
+//3, 4
+let letters = "qwertyuiop"
+console.log(letters.length);
+console.log(letters[9]);
+//or console.log(letters.slice(9));
+//or console.log(letters.slice(-1));
+//or console.log(letters[letters.length - 1]);
